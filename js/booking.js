9774c837afa96fdde662a026834a969c085c3bbc
duarteCost <2084813@student.uma.pt>
2017-11-26 19:07:23 +0000

    Post booking done --see booking.js

--- js/booking.js
+++ js/booking.js
@@ -8,18 +8,44 @@ function get_all_booking(){
 }
 
 
+
 $( document ).ready(function() {
 
 	$.support.cors = true;
 
 	$( "#bookingCreate" ).click(function(e)
-		{
-		  $("#dinamicContent").load("bookingForm.html"); //mudar este path no futuro
-		  e.preventDefault();
+	{
+	  $("#dinamicContent").load("bookingForm.html");
+	  e.preventDefault();
 	});
 
-	/*get_all_booking().done(function(data){
-		console.log(data);
-	});*/
+
+    $( "#showBookings" ).click(function(e)
+    {
+        $("#dinamicContent").load("showBookings.html");
+        e.preventDefault();
+
+        //$("#all").val("hello");
+    });
+
+    $('#addBooking').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: "PUT",
+            url: "http://127.0.0.1:5000/booking",
+            data: $(this).serializeArray(),
+
+            success: function (data) {
+                console.log('Submission was successful.');
+                console.log(data);
+            },
+            error: function (data) {
+                console.log('An error occurred.');
+                console.log(data);
+            }
+        });
+    });
+
+
 
 });
