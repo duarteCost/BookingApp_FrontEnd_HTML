90d91865128a4985877945c8e80266eb10a89b9e
filipaNobrega <filipanobrega1994@gmail.com>
2017-12-04 12:23:13 +0000

    Update login

--- js/userlogin.js
+++ js/userlogin.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
     $('#login-form').submit(function signup(e){
         e.preventDefault();
         $.ajax({
-            method: "GET",
+            method: "POST",
             url: "http://127.0.0.1:5000/user/login",
             data: $(this).serializeArray(),
             success: function (data) {
                 console.log('Submission was successful.');
                 console.log(data);
-                location.replace('index.html')
+                location.replace('index.html')                
             },
             error: function (data) {
                 $('#error-message').html('O email e/ou password introduzidos estão incorrectos.');
